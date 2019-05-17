189d57e85213141e6f9e112f920e30a1373f327f
Sal Castoro <salcastoro@gmail.com>
2019-05-17 17:55:10 -0400

    implemented function structure to cost_converter, created simple gui to improve user experience

--- cost_converter.py
+++ cost_converter.py
@@ -1,31 +1,43 @@
-codex = {
-    "A": "0",
-    "B": "1",
-    "C": "2",
-    "D": "3",
-    "E": "4",
-    "F": "5",
-    "G": "6",
-    "H": "7",
-    "I": "8",
-    "J": "9"
-}
 
-i = input('Type Here: ')
+def cost_converter(codeInput):
+    codex = {
+        "A": "0",
+        "B": "1",
+        "C": "2",
+        "D": "3",
+        "E": "4",
+        "F": "5",
+        "G": "6",
+        "H": "7",
+        "I": "8",
+        "J": "9"
+    }
 
-if ',' in i:
-    x = i.split(',')
-    totalCost = 0
-    for code in x:
+    if ',' in codeInput:
+        x = codeInput.split(',')
+        totalCost = 0
+        for code in x:
+            cost = ""
+            try:
+                for letter in code:
+                    if letter == ' ':
+                        continue
+                    else:
+                        cost += codex[letter.upper()]
+            except KeyError:
+                return f"{letter} is incorrect"
+            totalCost += int(cost)
+        return totalCost
+    else:
         cost = ""
-        for letter in code:
-            cost += codex[letter]
-        totalCost += int(cost)
-    print(totalCost)
-else:
-    cost = ""
-    totalCost = 0
-    for letter in i:
-        cost += codex[letter]
-    totalCost = int(cost)
-    print(totalCost)
+        totalCost = 0
+        try:
+            for letter in codeInput:
+                if letter == ' ':
+                    continue
+                else:
+                    cost += codex[letter.upper()]
+        except KeyError:
+            return f"{letter} is incorrect"
+        totalCost = int(cost)
+        return totalCost
--- cost_window.py
+++ cost_window.py
@@ -1,18 +1,22 @@
 import PySimpleGUI as sg
+from cost_converter import cost_converter
 
-layout = [[sg.Text('Your typed chars appear here:'), sg.Text('', key='_OUTPUT_')],
-          [sg.Input(do_not_clear=True, key='_IN_')],
-          [sg.Button('Show'), sg.Button('Exit')]]
+layout = [[sg.Input(size=(30, 1), do_not_clear=False, key='_IN_', font=("Helvetica", 15))],
+          [sg.Text('Total Cost:', font=("Helvetica", 20))],
+          [sg.Output(size=(25, 2), font=("Helvetica", 20), key='_OUTPUT_')],
+          [sg.Button('Convert', bind_return_key=True), sg.Button('Exit')]]
 
-window = sg.Window('Window Title', layout)
+window = sg.Window('Convert Costs', layout)
 
-while True:                 # Event Loop
+while True:  # Event Loop
     event, values = window.Read()
     print(event, values)
     if event is None or event == 'Exit':
         break
-    if event == 'Show':
-        # change the "output" element to be the value of "input" element
-        window.Element('_OUTPUT_').Update(values['_IN_'])
+    if event == 'Convert':
+        print(values['_IN_'])
+        converted = cost_converter(values['_IN_'])
+        window.Element('_OUTPUT_').Update(value=converted)
+
 
 window.Close()
